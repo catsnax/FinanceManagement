5698fb600d689f96a97ef82f0922c7979c6c854a
ajjmacias@addu.edu.ph <ajjmacias@addu.edu.ph>
2025-07-11 19:59:24 +0800

    feat: created managers for different transaction types

--- back-end/api/finances/models.py
+++ back-end/api/finances/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class IncomeManager(models.Manager):
+    def get_queryset(self):
+        return(super().get_queryset().filter(transaction_type = Transaction.TransactionType.INCOME))
+
+class ExpenseManager(models.Manager):
+    def get_queryset(self):
+        return(super().get_queryset().filter(transaction_type = Transaction.TransactionType.EXPENSE))
+
+class InvestmentManager(models.Manager):
+    def get_queryset(self):
+        return(super().get_queryset().filter(transaction_type = Transaction.TransactionType.INVESTMENT))
+
 class Transaction(models.Model):
     class TransactionType(models.TextChoices):
         INCOME = 'IN', 'Income'
@@ -14,6 +26,9 @@ class Transaction(models.Model):
     updated = models.DateTimeField(auto_now = True)
     notes = models.TextField(blank=True, null = True)
     transaction_type = models.CharField(max_length = 2, choices = TransactionType)
+
+    objects = models.Manager()
+    income = IncomeManager()
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = 'finances_transactions')
 
